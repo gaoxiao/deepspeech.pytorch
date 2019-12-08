6d34e068df41798ad1d7b747afabb0c8e9e25dbb
gaoxiao <gaoxiao1988@gmail.com>
2019-12-08 10:53:19 -0800

    cmd gen tool

--- tools/gen_comand.py
+++ tools/gen_comand.py
@@ -1,11 +1,21 @@
 if __name__ == '__main__':
-    input = '2	Transformer	0.1	1.00E-03	0.5	2048	2	2'
+    input = '14	Transformer	0.03	1.00E-04	0.5	1024	2	2	16	6 + Mask	Best: 0.835																						'
     arr = input.split()
     id = arr[0]
     lr = arr[2]
     decay = arr[3]
     dropout = arr[4]
-    cuda = int(id) % 4
-    cmd = 'CUDA_VISIBLE_DEVICES={cuda} python train_tf.py --tensorboard --cuda --lr={lr} --decay={decay}'.format(
-        cuda=1, lr=lr, decay=decay)
+    hidden_size = arr[5]
+    hidden_layers = arr[6]
+    fc_layers = arr[7]
+    decoder_output = arr[8]
+    cuda = int(id) % 3 + 1
+    cmd = 'CUDA_VISIBLE_DEVICES={cuda} python train_tf.py ' \
+          '--tensorboard --cuda --lr={lr} --decay={decay} ' \
+          '--hidden-layers={hidden_layers} --hidden-size={hidden_size} ' \
+          '--tf-decoder-output={decoder_output} --fc-layers={fc_layers} ' \
+          '--id={id}'.format(
+        cuda=cuda, lr=lr, decay=decay,
+        hidden_layers=hidden_layers, hidden_size=hidden_size,
+        decoder_output=decoder_output, fc_layers=fc_layers, id=id)
     print(cmd)
