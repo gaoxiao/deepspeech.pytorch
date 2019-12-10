cdc692f050413b1663ce50362215a1aab21a08d3
Xiao Gao <gaoxiao1988@gmail.com>
2019-12-09 22:25:15 -0800

    Add test threshold F1.

--- tcn/test.py
+++ tcn/test.py
@@ -1,6 +1,7 @@
 import argparse
 
 import torch
+from sklearn.metrics import classification_report
 from tqdm import tqdm
 
 from data.cp_data_loader import SpectrogramDataset, AudioDataLoader
@@ -73,6 +74,39 @@ def evaluate_class(test_loader, device, model):
     return corr.item()
 
 
+def concat(all, new):
+    if all is None:
+        return new
+    else:
+        return torch.cat((all, new))
+
+
+def evaluate_parent(test_loader, device, model):
+    model.eval()
+    corr = 0
+    all_idx = None
+    all_targets = None
+    threshold = 0.999
+    for i, (data) in tqdm(enumerate(test_loader), total=len(test_loader)):
+        inputs, targets, input_percentages = data
+        input_sizes = input_percentages.mul_(int(inputs.size(3))).int()
+        inputs = inputs.to(device)
+        targets = targets.long().to(device)
+
+        out, hidden, output_sizes = model(inputs, input_sizes)
+        hidden_out = hidden.float()  # ensure float32 for loss
+
+        threshold_parent_idx = (hidden_out[:, 1] > threshold).long()
+
+        corr += torch.sum(threshold_parent_idx == targets)
+
+        all_idx = concat(all_idx, threshold_parent_idx)
+        all_targets = concat(all_targets, targets)
+
+    print(classification_report(all_targets.cpu(), all_idx.cpu(), target_names=['0', '1', '2']))
+    return corr.item()
+
+
 if __name__ == '__main__':
     args = parser.parse_args()
     torch.set_grad_enabled(False)
@@ -83,9 +117,9 @@ if __name__ == '__main__':
                                       labels=model.labels, normalize=True)
     test_loader = AudioDataLoader(test_dataset, batch_size=args.batch_size,
                                   num_workers=args.num_workers)
-    corr = evaluate_class(test_loader=test_loader,
-                          device=device,
-                          model=model)
+    corr = evaluate_parent(test_loader=test_loader,
+                           device=device,
+                           model=model)
 
     size = float(len(test_dataset))
     print(corr, size, corr / size)
